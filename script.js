4adcc1e3c4b3a4044c2a5d8f442d3efb2aebc257
Pratik Mane <manepratik3727@gmail.com>
2025-11-26 23:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const image1=document.querSelector("#div1")
+const image2=document.querSelector("#div2")
+const image3=document.querSelector("#div3")
+const image4=document.querSelector("#div4")
+const image5=document.querSelector("#div5")
+const image6=document.querSelector("#div6")
+
+const main=document.querSelector("#parent")
+main.innerHtml=
