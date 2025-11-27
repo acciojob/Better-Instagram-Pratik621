4a9e9d9fef1157690321e74febffc95758470291
Pratik Mane <manepratik3727@gmail.com>
2025-11-27 17:12:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ let store = null;
 images.forEach((img) => {
   img.addEventListener("dragstart", (e) => {
     store = e.target;
-    e.target.classList.add("dragging");
+    
   });
 
-  img.addEventListener("dragend", (e) => {
-    e.target.classList.remove("dragging");
-  });
 
   img.addEventListener("dragover", (e) => {
     e.preventDefault();
