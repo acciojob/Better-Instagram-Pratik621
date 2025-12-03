6aa202272b0d9477555e90dfd9297f5979182ab9
Pratik Mane <manepratik3727@gmail.com>
2025-12-03 22:22:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-const images = document.querySelectorAll(".image");
-let store;
+//your code here
+let dragElement=null
+document.addEventListener("dragstart",(e)=>{
+	dragElement=e.target
+})
+document.addEventListener("dragover",(e)=>{
+	e.preventDefault();
+})
+document.addEventListener("drop",(e)=>{
+	e.preventDefault();
+	let target=e.target;
 
-images.forEach((img) => {
-  img.addEventListener("dragstart", (e) => {
-    store = e.target;
-  });
-
-
-  img.addEventListener("dragover", (e) => {
-    e.preventDefault();
-  });
-
-  img.addEventListener("drop", (e) => {
-    e.preventDefault();
-    const target = e.target;
-
-    if (store !== target) {
-		
-      const storeNext = store.nextSibling;
-      const targetNext = target.nextSibling;
-      
-      store.parentNode.insertBefore(target, storeNext);
-      target.parentNode.insertBefore(store, targetNext);
-    }
-  });
-});
+	if(target!=dragElement && target.parentElement.id=="parent"){
+			let temp=document.createElement("div");
+			target.replaceWith(temp);
+			dragElement.replaceWith(target);
+			temp.replaceWith(dragElement);
+	}
+})
